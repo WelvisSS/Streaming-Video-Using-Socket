12c2ea25081b5d1a9a5e1b4cb18682ef508f7fe2
Fabiano Santos <fabio_fss@users.noreply.github.com>
2021-11-14 12:41:37 -0300

    changes to overall structure

--- cliente.py
+++ cliente.py
@@ -1,31 +1,78 @@
 from socket import *
 import cv2 as cv
 import numpy as np
+import os
 
-def show_image(image):
-    image = np.asarray(bytearray(image), np.uint8)
-    image = cv.imdecode(image, cv.IMREAD_COLOR)
-    cv.imshow('image', image)
-    cv.waitKey(0)
+class ClientSocket:
+    def __init__(self, name='localhost', port=7777) -> None:
+        self.setup_socket(name, port)
+
+    def setup_socket(self, name, port) -> None:
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((name, port))
+    
+    def get_image(self):
+        print('Obtendo imagem...')
+        self.socket.send(b'get_image')
+
+        frame = self.socket.recv(1024*10000)
+        print('Exibindo imagem...')
+        self.show_image(frame, 0)
+
+        self.socket.close()
+        print('Finalizado.')
+
+    def get_video(self):
+        print('Obtendo video...')
+        self.socket.send(b'get_video')
+
+        print('Reproduzindo video...')
+        while True:
+            frame = self.socket.recv(1024*10000)
+
+            if frame == b'end':
+                break
+
+            self.show_image(frame, 1)
+
+        cv.destroyAllWindows()
+        self.socket.close()
+        print('Finalizado.')
+
+    def show_image(self, image, key=0):
+        image = np.asarray(bytearray(image), np.uint8)
+        image = cv.imdecode(image, cv.IMREAD_COLOR)
+        cv.imshow('image', image)
+        cv.waitKey(key)
 
 
-server_name = 'localhost'
-server_port = 7777
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((server_name, server_port))
 
-# client_socket.send(b'get_image')
-# frame = client_socket.recv(1024*10000)
-# show_image(frame)
 
-client_socket.send(b'get_video')
-while True:
-    frame = client_socket.recv(1024*10000)
+if __name__ == '__main__':
+    choice = None
 
-    if frame == b'end':
-        break
+    while choice != '3':
+        os.system('cls')
+        print('------------')
+        print('1- Imagem')
+        print('2- Video')
+        print('3- Cancelar')
+        print('------------\n')
 
-    print('recebido')
-    show_image(frame)
+        choice = input('Selecione uma opção: ')
 
-client_socket.close()
+        if choice == '1':
+            client_socket = ClientSocket('localhost', 7777)
+            client_socket.get_image()
+
+        elif choice == '2':
+            client_socket = ClientSocket('localhost', 7777)
+            client_socket.get_video()
+
+
+
+
+
+
+
+
